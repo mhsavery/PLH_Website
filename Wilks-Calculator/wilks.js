93df6f0fd43eb42be7a65acb9e328f90dec3ff02
Matthew Savery <mhsavery126@gmail.com>
2015-02-21 00:53:01 -0500

    lbs calculation nearly complete
    comments in file outlining what is left to be done

--- Wilks-Calculator/wilks.js
+++ Wilks-Calculator/wilks.js
@@ -1,74 +1,52 @@
 $(document).ready(function () {
     $("#bodyweight").focus();
+});
 
-    $(".submits").keyup(function (enter) {
-        if (enter.keyCode == 13) {
-            wilks();
-        }
-    });
-
-    $("#findValue").click(function (enter) {
-        enter.preventDefault();
+$(".submits").keyup(function (enter) {
+    if (enter.keyCode == 13) { //enter
         wilks();
-    });
+    }
+});
 
+$("#findValue").click(function (enter) {
+    enter.preventDefault();
+    wilks();
 });
 
 function wilks(){
         //Get value of gender input
-        var gen = $('input[name="gender"]:checked').val();
+        var gender = $('input[name="gender"]:checked').val();
+        var gen = gender;
 
         //Get value of unit of measurement input
         var unit = $('input[name="unit"]:checked').val();
+        var un = unit;
 
         //Get bodyweight value
-        var bWeight = $('#bodyweight').val();
+        var bodyweight = $('#bodyweight').val();
+        var bWeight = bodyweight;
 
         //Get amount of weight lifted
-        var lWeight = $('#liftedweight').val();
+        var liftedweight = $('#liftedweight').val();
+        var lWeight = liftedweight;
 
         //Declare wilks value variable
         var wilks = 0;
 
+
+        /*
+        Need to build 2 arrays for male / female using chart found online with coefficients.
+        Kilogram values will be tricky to match up.
+        Weights between 40 - 206 pounds
+        Each weight matches with a specific coefficient
+        Wilks = total lifted * the coefficient / 2.20462
+        */
+
+
+        //Test run. Coefficient is for 150 pound male.
+        var cf = .7661;
         
-        if (gen == 1) {
-
-            //Coefficients for men
-            a=-216.0475144;
-            b=16.2606339;
-            c=-0.002388645;
-            d=-0.00113732;
-            e=7.01863E-06;
-            f=-1.291E-08;
-
-        } else if(gen == 2){  
-
-            //Coefficients for women
-            a=594.31747775582;
-            b=-27.23842536447;
-            c=0.82112226871;
-            d=-0.00930733913;
-            e=0.00004731582;
-            f=-0.00000009054;
-
-        }
-
-        //Convert pounds into kilograms
-        if(unit == 1) {
-            (bWeight / 2.20462262).toFixed(2);
-            (lWeight / 2.20462262).toFixed(2);
-        }
-
-
-        //Calculate wilks value
-        wilks = lWeight*(500/(a+(b*bWeight)+
-                (c*Math.pow(bWeight,2))+
-                (d*Math.pow(bWeight,3))+
-                (e*Math.pow(bWeight,4))+
-                (f*Math.pow(bWeight,5)))); 
-
-        //Round Wilks to 4 places
-        wilks = wilks.toFixed(4);
+        wilks = ((lWeight * .7661) / 2.20462).toFixed(2);
 
         $("#result").html(wilks);
 
